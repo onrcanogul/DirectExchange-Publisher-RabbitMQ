1f6d0bc076db073a0c54b9f2b00749325da5c568
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2024-04-25 14:27:46 +0300

    Add project files.

---
+++ Publisher/Class1.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://yejguzib:NuvHgMGxrkfxK8SHh3VRyAYLi3B3ENf1@shark.rmq.cloudamqp.com/yejguzib");
+
+//Bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "example-queue",exclusive:false);
+
+//Queue' a mesaj gönderme
+//RabbitMQ kuyruga atacagı mesajları byte türünden kabul etmektedir.
+byte[] message = Encoding.UTF8.GetBytes("merhaba");
+channel.BasicPublish("", routingKey: "example-queue", body: message); //"" => default exchange
+
+
+Console.Read();
+
+
+
+
+
+            
+    
+
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://yejguzib:NuvHgMGxrkfxK8SHh3VRyAYLi3B3ENf1@shark.rmq.cloudamqp.com/yejguzib");
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+channel.ExchangeDeclare(exchange: "direct-exchange-example", ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Message:  ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "direct-exchange-example",
+        routingKey: "direct-queue-example",
+        body: byteMessage);
+}
+
+Console.Read();
+
+
